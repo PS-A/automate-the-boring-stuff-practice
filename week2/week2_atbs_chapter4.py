063a43222e1c8a3b708efdf6dbd22c66f8c12496
PeePa <petteri.suur-askola@gantti.fi>
2025-05-25 20:22:58 +0300

    Week 2 'Automate the Boring Stuff with Python chapter 5 and chapter 6 practices.

--- week2/week2_atbs_chapter4.py
+++ week2/week2_atbs_chapter4.py
@@ -2,46 +2,46 @@
 """
 catNames = []
 while True:
-    print('Enter the name of cat ' + str(len(catNames) + 1) +
-      ' (Or enter nothing to stop.):')
+    print("Enter the name of cat " + str(len(catNames) + 1) +
+      " (Or enter nothing to stop.):")
     name = input()
-    if name == '':
+    if name == "":
         break
     catNames = catNames + [name]  # list concatenation
-print('The cat names are:')
+print("The cat names are:")
 for name in catNames:
-    print('  ' + name)
+    print("  " + name)
 """
 
 # In/Not in testing.
 """
-myPets = ['Zophie', 'Pooka', 'Fat-tail']
-print('Enter a pet name:')
+myPets = ["Zophie", "Pooka", "Fat-tail"]
+print("Enter a pet name:")
 name = input()
 if name not in myPets:
-    print('I do not have a pet named ' + name)
+    print("I do not have a pet named " + name)
 else:
-    print(name + ' is my pet.')
+    print(name + " is my pet.")
 """
 
 # Magic 8 ball with a list.
 """
 import random
 
-messages = ['It is certain',
-    'It is decidedly so',
-    'Yes definitely',
-    'Reply hazy try again',
-    'Ask again later',
-    'Concentrate and ask again',
-    'My reply is no',
-    'Outlook not so good',
-    'Very doubtful']
+messages = ["It is certain",
+    "It is decidedly so",
+    "Yes definitely",
+    "Reply hazy try again",
+    "Ask again later",
+    "Concentrate and ask again",
+    "My reply is no",
+    "Outlook not so good",
+    "Very doubtful"]
 
 print(messages[random.randint(0, len(messages) - 1)])
 """
 
-# Conway's Game of Life
+# Conway"s Game of Life
 """
 import random, time, copy
 WIDTH = 60
@@ -53,22 +53,22 @@ for x in range(WIDTH):
     column = [] # Create a new column.
     for y in range(HEIGHT):
         if random.randint(0, 1) == 0:
-            column.append('#') # Add a living cell.
+            column.append("#") # Add a living cell.
         else:
-            column.append(' ') # Add a dead cell.
+            column.append(" ") # Add a dead cell.
     nextCells.append(column) # nextCells is a list of column lists.
 
 while True: # Main program loop.
-    print('\n\n\n\n\n') # Separate each step with newlines.
+    print("\n\n\n\n\n") # Separate each step with newlines.
     currentCells = copy.deepcopy(nextCells)
 
     # Print currentCells on the screen:
     for y in range(HEIGHT):
         for x in range(WIDTH):
-            print(currentCells[x][y], end='') # Print the # or space.
+            print(currentCells[x][y], end="") # Print the # or space.
         print() # Print a newline at the end of the row.
 
-    # Calculate the next step's cells based on current step's cells:
+    # Calculate the next step"s cells based on current step"s cells:
     for x in range(WIDTH):
         for y in range(HEIGHT):
             # Get neighboring coordinates:
@@ -80,34 +80,34 @@ while True: # Main program loop.
 
             # Count number of living neighbors:
             numNeighbors = 0
-            if currentCells[leftCoord][aboveCoord] == '#':
+            if currentCells[leftCoord][aboveCoord] == "#":
                 numNeighbors += 1 # Top-left neighbor is alive.
-            if currentCells[x][aboveCoord] == '#':
+            if currentCells[x][aboveCoord] == "#":
                 numNeighbors += 1 # Top neighbor is alive.
-            if currentCells[rightCoord][aboveCoord] == '#':
+            if currentCells[rightCoord][aboveCoord] == "#":
                 numNeighbors += 1 # Top-right neighbor is alive.
-            if currentCells[leftCoord][y] == '#':
+            if currentCells[leftCoord][y] == "#":
                 numNeighbors += 1 # Left neighbor is alive.
-            if currentCells[rightCoord][y] == '#':
+            if currentCells[rightCoord][y] == "#":
                 numNeighbors += 1 # Right neighbor is alive.
-            if currentCells[leftCoord][belowCoord] == '#':
+            if currentCells[leftCoord][belowCoord] == "#":
                 numNeighbors += 1 # Bottom-left neighbor is alive.
-            if currentCells[x][belowCoord] == '#':
+            if currentCells[x][belowCoord] == "#":
                 numNeighbors += 1 # Bottom neighbor is alive.
-            if currentCells[rightCoord][belowCoord] == '#':
+            if currentCells[rightCoord][belowCoord] == "#":
                 numNeighbors += 1 # Bottom-right neighbor is alive.
 
-            # Set cell based on Conway's Game of Life rules:
-            if currentCells[x][y] == '#' and (numNeighbors == 2 or
+            # Set cell based on Conway"s Game of Life rules:
+            if currentCells[x][y] == "#" and (numNeighbors == 2 or
 numNeighbors == 3):
                 # Living cells with 2 or 3 neighbors stay alive:
-                nextCells[x][y] = '#'
-            elif currentCells[x][y] == ' ' and numNeighbors == 3:
+                nextCells[x][y] = "#"
+            elif currentCells[x][y] == " " and numNeighbors == 3:
                 # Dead cells with 3 neighbors become alive:
-                nextCells[x][y] = '#'
+                nextCells[x][y] = "#"
             else:
                 # Everything else dies or stays dead:
-                nextCells[x][y] = ' '
+                nextCells[x][y] = " "
     time.sleep(1) # Add a 1-second pause to reduce flickering.
 """
 
@@ -137,7 +137,7 @@ import random
 numberOfStreaks = 0
 
 for experimentNumber in range(10000):
-    # Code that creates a list of 100 'heads' or 'tails' values.
+    # Code that creates a list of 100 "heads" or "tails" values.
     heads_or_tails = []
     for i in range(100):
         flip = random.randint(0, 1)
@@ -159,20 +159,20 @@ for experimentNumber in range(10000):
             numberOfStreaks += 1
             break
 
-print('Chance of streak: %s%%' % (numberOfStreaks / 100))
+print("Chance of streak: %s%%" % (numberOfStreaks / 100))
 """
 
 # Character Picture Grid
 
-grid = [['.', '.', '.', '.', '.', '.'],
-        ['.', 'O', 'O', '.', '.', '.'],
-        ['O', 'O', 'O', 'O', '.', '.'],
-        ['O', 'O', 'O', 'O', 'O', '.'],
-        ['.', 'O', 'O', 'O', 'O', 'O'],
-        ['O', 'O', 'O', 'O', 'O', '.'],
-        ['O', 'O', 'O', 'O', '.', '.'],
-        ['.', 'O', 'O', '.', '.', '.'],
-        ['.', '.', '.', '.', '.', '.']]
+grid = [[".", ".", ".", ".", ".", "."],
+        [".", "O", "O", ".", ".", "."],
+        ["O", "O", "O", "O", ".", "."],
+        ["O", "O", "O", "O", "O", "."],
+        [".", "O", "O", "O", "O", "O"],
+        ["O", "O", "O", "O", "O", "."],
+        ["O", "O", "O", "O", ".", "."],
+        [".", "O", "O", ".", ".", "."],
+        [".", ".", ".", ".", ".", "."]]
 
 for x in range(6):
     for y in range(9):
