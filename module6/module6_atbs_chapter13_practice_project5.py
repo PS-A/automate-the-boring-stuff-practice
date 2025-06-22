e1c6af34e041ffd4e401f8bba539ded46f7a9518
PeePa <petteri.suur-askola@gantti.fi>
2025-06-22 10:15:40 +0300

    Working through ATBS Chapter 13 related to Excel.

---
+++ module6/module6_atbs_chapter13_practice_project5.py
@@ -0,0 +1,19 @@
+# Spreadsheets to Text Files
+
+import openpyxl
+
+def text_to_spreadsheets(filename):
+    wb = openpyxl.load_workbook(filename)
+    sheet = wb.active
+    excel_data = []
+
+    for row in sheet.iter_rows(values_only=True): # type:ignore
+        excel_data.append(list(row))
+    transposed_data = list(zip(*excel_data))
+
+    for i, row in enumerate(transposed_data, start=1):
+        with open(f"spreadsheet{i}.txt", "w") as file:
+            for j, value in enumerate(row, start=1):
+                file.write(str(value) + "\n")
+
+text_to_spreadsheets("text_to_spreadsheets.xlsx")
