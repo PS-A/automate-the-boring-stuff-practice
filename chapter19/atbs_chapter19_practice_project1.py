15c930dca49dbe142e8a066dc23a23c528b0e262
PeePa <petteri.suur-askola@gantti.fi>
2025-08-16 10:22:48 +0300

    ATBS Chapter 19 practice, ongoing.

---
+++ chapter19/atbs_chapter19_practice_project1.py
@@ -0,0 +1,45 @@
+# Extending and Fixing the Chapter Project Programs
+
+import os
+from PIL import Image
+
+SQUARE_FIT_SIZE = 300
+LOGO_FILENAME = 'catlogo.png'
+
+logoIm = Image.open(LOGO_FILENAME)
+logoWidth, logoHeight = logoIm.size
+
+os.makedirs('withLogo', exist_ok=True)
+# Loop over all files in the working directory.
+for filename in os.listdir('.'):
+    if not (filename.lower().endswith('.png') or filename.lower().endswith('.jpg') \
+            or filename.lower().endswith('.gif') or filename.lower().endswith('.bmp')) \
+            or filename == LOGO_FILENAME:
+        continue    # skip non-image files and the logo file itself
+
+    im = Image.open(filename)
+    width, height = im.size
+
+    if not (width > logoWidth*2 and height > logoHeight*2):
+        continue # skip if image not twice as big as the logo
+
+    # Check if image needs to be resized.
+    if width > SQUARE_FIT_SIZE and height > SQUARE_FIT_SIZE:
+        # Calculate the new width and height to resize to.
+        if width > height:
+            height = int((SQUARE_FIT_SIZE / width) * height)
+            width = SQUARE_FIT_SIZE
+        else:
+            width = int((SQUARE_FIT_SIZE / height) * width)
+            height = SQUARE_FIT_SIZE
+
+        # Resize the image.
+        print('Resizing %s...' % (filename))
+    im = im.resize((width, height))
+
+     # Add the logo.
+    print('Adding logo to %s...' % (filename))
+    im.paste(logoIm, (width - logoWidth, height - logoHeight), logoIm)
+
+     # Save changes.
+    im.save(os.path.join('withLogo', filename))
