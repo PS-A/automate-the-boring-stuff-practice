8b7d22da74ee8f2a2f9d31849949122ebf882eb0
PeePa <petteri.suur-askola@gantti.fi>
2025-08-17 08:32:26 +0300

    Working through ATBS Chapter 20, ongoing.

---
+++ chapter20/atbs_chapter20.py
@@ -0,0 +1,42 @@
+# Spiral Draw
+"""
+import pyautogui, time
+time.sleep(5)
+pyautogui.click()    # Click to make the window active.
+distance = 300
+change = 20
+while distance > 0:
+    pyautogui.drag(distance, 0, duration=0.2)   # Move right.
+    distance = distance - change
+    pyautogui.drag(0, distance, duration=0.2)   # Move down.
+    pyautogui.drag(-distance, 0, duration=0.2)  # Move left.
+    distance = distance - change
+    pyautogui.drag(0, -distance, duration=0.2)  # Move up.
+"""
+
+# Form Filler
+
+#! python3
+# Automatically fills in the form.
+
+import pyautogui, time
+
+# TODO: Give the user a chance to kill the script.
+
+# TODO: Wait until the form page has loaded.
+
+# TODO: Fill out the Name Field.
+
+# TODO: Fill out the Greatest Fear(s) field.
+
+# TODO: Fill out the Source of Wizard Powers field.
+
+# TODO: Fill out the RoboCop field.
+
+# TODO: Fill out the Additional Comments field.
+
+# TODO: Click Submit.
+
+# TODO: Wait until form page has loaded.
+
+# TODO: Click the Submit another response link.
